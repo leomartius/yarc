db8a3381dcda27e5f1a60512d0057b6d1574e4cd
Leo Martin <leomartius@pm.me>
2024-09-09 20:37:36 +0000

    Use lazy imports in the main module

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import logging
 from pathlib import Path
 from typing import Never
 
-from game.main_menu import show_menu
-from game.version import version_string
-
 ASSETS = Path('assets')
 SAVEFILE = Path('yarc.sav')
 
@@ -15,7 +12,11 @@ SAVEFILE = Path('yarc.sav')
 def main() -> Never:
     if __debug__:
         logging.basicConfig(level=logging.DEBUG)
+    # Delay importing the version module until after logging is configured.
+    from game.version import version_string
     print(f"Y.A.R.C. version {version_string.lstrip('v')}")
+    # Import game modules after basic initialization.
+    from game.main_menu import show_menu
     install_dir = Path(__file__).parent
     show_menu(install_dir / ASSETS, SAVEFILE)
 
