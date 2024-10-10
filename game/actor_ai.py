88ed1727fb3dd4673003d42f6fcc421978399ec0
Leo Martin <leomartius@pm.me>
2024-10-10 21:32:27 +0000

    Refactor hostile monster AI

--- game/actor_ai.py
+++ game/actor_ai.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import random
 
 from game.action import Action, MeleeAction, MoveAction, WaitAction
 from game.constants import Tile
@@ -64,40 +65,18 @@ class MeanAI(ActorAI):
 # chase and attack the player
 class HostileAI(ActorAI):
     @staticmethod
-    def _distance(x1: int, y1: int, x2: int, y2: int) -> int:
-        return (x1 - x2) ** 2 + (y1 - y2) ** 2
-
-    @staticmethod
     def _find_nearest_door(goal_x: int, goal_y: int, level: Level,
                            x1: int, y1: int, x2: int, y2: int) -> tuple[int, int] | None:
         door = None
-        distance = HostileAI._distance(0, 0, level.width, level.height)
+        distance = _distance(0, 0, level.width, level.height)
         for i in range(x1, x2 + 1):
             for j in range(y1, y2 + 1):
                 if level.tiles[i, j] == Tile.DOOR:
-                    new_dist = HostileAI._distance(i, j, goal_x, goal_y)
+                    new_dist = _distance(i, j, goal_x, goal_y)
                     if new_dist < distance:
                         door = (i, j)
                         distance = new_dist
         return door
-
-    @staticmethod
-    def _approach(actor: Actor, goal_x: int, goal_y: int, level: Level) -> Action:
-        dest_x, dest_y = actor.x, actor.y
-        distance = HostileAI._distance(actor.x, actor.y, goal_x, goal_y)
-        for i in range(actor.x - 1, actor.x + 2):
-            for j in range(actor.y - 1, actor.y + 2):
-                if (level.in_bounds(i, j) and level.is_walkable(i, j)
-                        and level.is_connected(actor.x, actor.y, i, j)
-                        and not level.get_actor_at(i, j)):
-                    new_dist = HostileAI._distance(i, j, goal_x, goal_y)
-                    if new_dist < distance:
-                        dest_x, dest_y = i, j
-                        distance = new_dist
-        if (dest_x, dest_y) == (actor.x, actor.y):
-            return WaitAction()
-        else:
-            return MoveAction(dest_x - actor.x, dest_y - actor.y)
 
     def take_turn(self, actor: Actor, level: Level, player: Actor) -> Action:
         if -1 <= player.x - actor.x <= +1 and -1 <= player.y - actor.y <= +1:
@@ -108,8 +87,38 @@ class HostileAI(ActorAI):
                 if not (x1 <= player.x <= x2 and y1 <= player.y <= y2):
                     door = HostileAI._find_nearest_door(player.x, player.y, level, x1, y1, x2, y2)
                     if door:
-                        return HostileAI._approach(actor, door[0], door[1], level)
-        return HostileAI._approach(actor, player.x, player.y, level)
+                        return _approach(actor, door[0], door[1], level)
+        return _approach(actor, player.x, player.y, level)
 
     def is_helpless(self) -> bool:
         return False
+
+
+# Euclidean square distance
+def _distance(x1: int, y1: int, x2: int, y2: int) -> int:
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+
+# Move to the nearby cell that minimizes the Euclidean distance.
+def _approach(actor: Actor, goal_x: int, goal_y: int, level: Level) -> Action:
+    destinations = [(actor.x, actor.y)]
+    distance = _distance(actor.x, actor.y, goal_x, goal_y)
+    for i in range(actor.x - 1, actor.x + 2):
+        for j in range(actor.y - 1, actor.y + 2):
+            if (
+                level.in_bounds(i, j)
+                and level.is_walkable(i, j)
+                and level.is_connected(actor.x, actor.y, i, j)
+                and not level.get_actor_at(i, j)
+            ):
+                new_dist = _distance(i, j, goal_x, goal_y)
+                if new_dist < distance:
+                    destinations = [(i, j)]
+                    distance = new_dist
+                elif new_dist == distance:
+                    destinations += [(i, j)]
+    if (actor.x, actor.y) in destinations:
+        return WaitAction()
+    else:
+        dest_x, dest_y = random.choice(destinations)
+        return MoveAction(dest_x - actor.x, dest_y - actor.y)
